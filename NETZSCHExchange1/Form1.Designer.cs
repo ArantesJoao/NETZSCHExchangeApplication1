1292ef57fc95d06b68137859055ac79c9f870781
João <arantesjoaov@gmail.com>
2023-09-28 16:23:30 -0300

    Add project files.

---
+++ NETZSCHExchange1/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace NETZSCHExchange1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.updateTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(51, 74);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(180, 20);
+            this.txtInput.TabIndex = 0;
+            this.txtInput.TextChanged += new System.EventHandler(this.txtInput_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(51, 140);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(180, 20);
+            this.txtOutput.TabIndex = 1;
+            // 
+            // updateTimer
+            // 
+            this.updateTimer.Enabled = true;
+            this.updateTimer.Interval = 1000;
+            this.updateTimer.Tick += new System.EventHandler(this.updateTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Timer updateTimer;
+    }
+}
+
---
+++ NETZSCHExchange1/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace NETZSCHExchange1
+{
+    public partial class Form1 : Form
+    {
+        private const string apiUrl = "http://localhost:3001/api/data";
+        private HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                var response = await client.GetStringAsync(apiUrl);
+                txtOutput.Text = response;
+            }
+            catch
+            {
+                // Handle connection errors, perhaps show a message to the user.
+            }
+        }
+
+        private async void updateTimer_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                var response = await client.GetStringAsync(apiUrl);
+                if (response != txtInput.Text)
+                {
+                    txtOutput.Text = response;
+                }
+            }
+            catch
+            {
+                // Handle connection errors.
+            }
+        }
+
+        private async void txtInput_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                var content = new StringContent("{\"data\":\"" + txtInput.Text + "\"}", Encoding.UTF8, "application/json");
+                await client.PostAsync(apiUrl, content);
+            }
+            catch
+            {
+                // Handle connection errors.
+            }
+        }
+    }
+}
