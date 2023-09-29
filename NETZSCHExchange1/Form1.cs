6f99018ba61adc7250ca2712d12bd6b540ab447b
João <arantesjoaov@gmail.com>
2023-09-29 15:02:13 -0300

    feat: created a reconnection function

--- NETZSCHExchange1/Form1.cs
+++ NETZSCHExchange1/Form1.cs
@@ -4,6 +4,7 @@ using System.Net.WebSockets;
 using System.Threading;
 using System.Windows.Forms;
 using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
 
 namespace NETZSCHExchange1
 {
@@ -11,6 +12,7 @@ namespace NETZSCHExchange1
     {
         private const string wsUrl = "ws://localhost:3001";
         private ClientWebSocket ws = new ClientWebSocket();
+        private bool isForcedChange = false;
 
         public Form1()
         {
@@ -19,36 +21,100 @@ namespace NETZSCHExchange1
 
         private async void Form1_Load(object sender, EventArgs e)
         {
-            await ws.ConnectAsync(new Uri(wsUrl), CancellationToken.None);
-            ListenForMessages();
+            await TryConnect();
+        }
+
+        private async Task TryConnect()
+        {
+            try
+            {
+                ws = new ClientWebSocket(); // reinitialize webSocket if needed
+                await ws.ConnectAsync(new Uri(wsUrl), CancellationToken.None);
+                ResetUI(); // reset UI after successful connection
+                ListenForMessages();
+            }
+            catch (Exception ex)
+            {
+                HandleConnectionError(ex);
+            }
+        }
+
+        private void ResetUI()
+        {
+            isForcedChange = true; // set flag before forcing the text change
+            txtInput.Text = "";
+            txtInput.ForeColor = System.Drawing.Color.Black; // reset text color
+            txtInput.Enabled = true; // enable txtInput
+            lblOutput.Text = ""; // reset output label
+            lblOutput.ForeColor = System.Drawing.Color.Black; // reset text color
+            isForcedChange = false; // reset flag after changing the text
         }
 
         private async void ListenForMessages()
         {
             var buffer = new ArraySegment<byte>(new byte[8192]);
-            while (ws.State == WebSocketState.Open)
+            try
             {
-                var received = await ws.ReceiveAsync(buffer, CancellationToken.None);
-                var message = Encoding.UTF8.GetString(buffer.Array, 0, received.Count);
-                var json = JObject.Parse(message);
-                if (json["source"].ToString() == "react")
+                while (ws.State == WebSocketState.Open)
                 {
-                    lblOutput.Text = json["data"].ToString();  // Changed from txtOutput.Text
+                    var received = await ws.ReceiveAsync(buffer, CancellationToken.None);
+                    var message = Encoding.UTF8.GetString(buffer.Array, 0, received.Count);
+                    var json = JObject.Parse(message);
+                    if (json["source"].ToString() == "react")
+                    {
+                        lblOutput.Text = json["data"].ToString();
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                HandleConnectionError(ex);
             }
         }
 
         private async void txtInput_TextChanged(object sender, EventArgs e)
         {
-            var inputValue = string.IsNullOrEmpty(txtInput.Text) ? " " : txtInput.Text;  // ensure to send empty space if !txtInput.Text
-            var json = new JObject
+            if (isForcedChange)
+                return;
+
+            try
             {
-                ["source"] = "net",
-                ["data"] = inputValue
-            };
-            var message = Encoding.UTF8.GetBytes(json.ToString());
-            Console.WriteLine("Sending from .NET:", json.ToString());
-            await ws.SendAsync(new ArraySegment<byte>(message), WebSocketMessageType.Text, true, CancellationToken.None);
+                var inputValue = string.IsNullOrEmpty(txtInput.Text) ? " " : txtInput.Text; // ensure to send empty space if !txtInput.Text
+                var json = new JObject
+                {
+                    ["source"] = "net",
+                    ["data"] = inputValue
+                };
+                var message = Encoding.UTF8.GetBytes(json.ToString());
+                Console.WriteLine("Sending from .NET:", json.ToString());
+                await ws.SendAsync(new ArraySegment<byte>(message), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            catch (Exception ex)
+            {
+                HandleConnectionError(ex);
+            }
+        }
+
+        private async void HandleConnectionError(Exception ex)
+        {
+            string errorMessage = "Error while connecting to the ";
+            if (ex is WebSocketException)
+            {
+                errorMessage += "WebSocket";
+            }
+            else
+            {
+                errorMessage += "server";
+            }
+
+            txtInput.Text = errorMessage;
+            txtInput.ForeColor = System.Drawing.Color.Red; // Set the text color to red
+            txtInput.Enabled = false; // Disable the txtInput
+
+            lblOutput.Text = errorMessage; // Assuming lblOutput is a Label control, if it's a TextBox, use txtOutput.Text
+            lblOutput.ForeColor = System.Drawing.Color.Red; // Set the text color to red
+
+            await TryConnect();
         }
     }
 }
